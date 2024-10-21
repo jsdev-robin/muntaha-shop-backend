f153d7fdd32480fb666a32499b5ee40386f9cb9b
jsdev-robin <jsdev.robin@gmail.com>
2024-10-21 15:12:19 +0600

    remove old way cors rule

--- app.ts
+++ app.ts
@@ -34,19 +34,19 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 // Express.js CORS setup
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend domain
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend domain
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With,content-type'
+//   );
+//   next();
+// });
 
 // Configure Cross-Origin Resource Sharing (CORS)
 app.use(
