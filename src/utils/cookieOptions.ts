c22e993bde7f9fd2f62c8b7ad2944992c330ffe3
jsdev-robin <jsdev.robin@gmail.com>
2024-10-21 15:24:36 +0600

    add sameSite value strict

--- src/utils/cookieOptions.ts
+++ src/utils/cookieOptions.ts
@@ -25,7 +25,7 @@ const refreshTokenExpires: number = parseInt(
  */
 const commonOptions = {
   httpOnly: true,
-  sameSite: EnvConfig.ISPRODUCTION ? ('none' as const) : ('lax' as const),
+  sameSite: EnvConfig.ISPRODUCTION ? ('strict' as const) : ('lax' as const),
   secure: EnvConfig.ISPRODUCTION,
 };
 
