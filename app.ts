8ef770c31f4fd54a1a8853276bbbb2b84a08b822
jsdev-robin <jsdev.robin@gmail.com>
2024-10-22 22:47:08 +0600

    remove limiter form router

--- app.ts
+++ app.ts
@@ -47,10 +47,9 @@ app.use(cookieParser());
 // Apply the rate limiting middleware to all requests.
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
 });
 
 app.use(limiter);
